e5693c0afa44992740738f47b4b03b5abfcfc6be
viniciusrcarvalho <viniciusrcarvalho@gmail.com>
2021-03-30 10:16:04 -0300

    first commit

---
+++ src/main/java/br/com/restaws/main.java
@@ -0,0 +1,15 @@
+package br.com.restaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class main {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(main.class, args);
+
+	}
+
+}
